c00b97231314635b10ac2d58ba5028c9f33cc599
minonix <martiniclement06@gmail.com>
2023-10-30 23:19:19 +0100

    il ne manque que le css

--- js/questionnaire.js
+++ js/questionnaire.js
@@ -1,5 +1,5 @@
-// Récupérez les éléments du formulaire et du bouton "Commencer"
 const quizForm = document.getElementById("quizForm");
+const textResult = document.getElementById("text_result");
 const startButton = document.getElementById("startButton");
 
 // Écoutez l'événement de clic sur le bouton "Commencer"
@@ -37,13 +37,12 @@ function updateButtons() {
     // Le bouton "Suivant" est désactivé si nous sommes sur la dernière question
     nextBtn.disabled = currentQuestion === questions.length - 1;
 
-    if (currentQuestion === questions.length - 1){
-        // Le bouton "Suivant" est retiré si nous sommes déjà sur la dernière question
+    if (currentQuestion === questions.length - 1) {
         nextBtn.classList.add("none");
-    }else{
-        // Le bouton "suivant" est réaffiché si nous sommes déjà sur la première question
+    } else {
         nextBtn.classList.remove("none");
     }
+    
 
 
 
@@ -99,13 +98,67 @@ submitBtn.addEventListener("click", () => {
 
     if (answers.length === questions.length) {
         textAlerte.innerHTML = ""
-        // Affiche les réponses si toutes les questions ont été répondues
-        alert("Réponses enregistrées : " + answers.join(", "));
+
+        var lastAnswer = answers.pop();
         
+        // Utilisation de la méthode 'reduce' pour calculer la somme
+        const total = answers.reduce((accumulator, currentValue) => {
+            // Convertir les valeurs actuelles en nombres (parseInt)
+            const currentNumber = parseFloat(currentValue, 10);
+            // Ajouter la valeur convertie à l'accumulateur
+            return accumulator + currentNumber;
+        }, 0);
+
+        if (total <= 10) {
+            var taux = 3;
+        }else if (total <= 15) {
+            var taux = 2.74;
+        }else if (total <= 25) {
+            var taux = 2.52;
+        }else if (total <= 33) {
+            var taux = 2.10;
+        }else {
+            var taux = 1.85;
+        }
+        var lastAnswer = parseFloat(lastAnswer, 10);
+        const result = taux + lastAnswer;
+
+        quizForm.classList.add('none')
+        textResult.classList.remove('none')
+        const divResult = document.getElementById("result");
+        divResult.innerHTML = "<h1>Vos frais s'élève à : " + result + "%</h1> <p>vous avez atteint le score de : <b>" + total + "/40</b> soit <b>" + total/2 +"/20</b>"
     } else {
         textAlerte.innerHTML = "il vous manque des réponse à cochet"
     }
 });
+
+const restartBtn = document.getElementById("restartBtn");
+
+restartBtn.addEventListener("click", () => {
+    // Masquez le résultat
+    textResult.classList.add("none");
+    
+    // Réinitialisez la question actuelle à 0
+    currentQuestion = 0;
+
+    // Décochez toutes les réponses
+    questions.forEach((question) => {
+        const inputs = question.querySelectorAll('input[type="radio"]');
+        inputs.forEach((input) => {
+            input.checked = false;
+        });
+    });
+
+    quizForm.classList.remove('none')
+    
+    // Affichez la première question
+    showQuestion(currentQuestion);
+
+    // Mettez à jour les boutons de navigation
+    updateButtons();
+});
+
+
 
 // Affiche la première question et met à jour les boutons de navigation
 showQuestion(currentQuestion);
